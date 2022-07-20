f1120d6c1250a7075cdad1d3f030d6c30bc7f886
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-07-20 23:06:13 +0530

    navbar-footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Home from './Components/Home';
+import Home from './Components/Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
      <Home/>
+   
     </div>
   );
 }
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Navbar from '../navbar/Navbar'
+
+import AllRoutes from '../Routes/AllRoutes'
+import Footer from '../footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+    <Navbar/>
+       <AllRoutes/>
+    {/* ................................. */}
+    <div >
+
+    </div>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Home
