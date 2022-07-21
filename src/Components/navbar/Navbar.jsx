daba6cccd10cdf3f97f783622dc964a3ef5d988f
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-07-21 16:36:27 +0530

    home page completed

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div 
     style={{display:"flex",
     boxShadow: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
-    padding:"20px"
+    padding:"20px", 
     }}>
         <Spacer/>
         <Box style={{width:"200px"}} cursor= "pointer">
