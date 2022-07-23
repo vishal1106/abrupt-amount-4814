46823c16cd19de3b562ba901fe686f9267c4ad16
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-07-23 15:25:06 +0530

    sign in page completed

--- src/Components/Routes/AllRoutes.jsx
+++ src/Components/Routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Home/Home'
+import Login from '../Pages/Login'
 
 import Sign from '../Pages/Sign'
 import Support from '../Support/Support'
@@ -15,6 +16,7 @@ const AllRoutes = () => {
             <Route path="/works" element={<Works/>}></Route>
             <Route path='/usecase' element={<Usecase/>}></Route>
             <Route path='/sign' element={<Sign/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
             <Route path='/support' element={<Support/>}></Route>
         </Routes>
     </div>
